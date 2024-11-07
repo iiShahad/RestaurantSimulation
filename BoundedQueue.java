7c4938baf5f344ef20e457d51d3a674d82cdd414
iishahad <shahad.m.ibrahem@gmail.com>
2024-11-07 11:48:00 +0300

    return table number

--- BoundedQueue.java
+++ BoundedQueue.java
@@ -7,6 +7,7 @@ class BoundedQueue<T>{
      //Constructor --------------------------------------------------------------------
     private final int maxSize;
     private final PriorityQueue<T> queue = new PriorityQueue<>();
+    private final int[] indices;
     private final CustomSemaphore consumingSemaphore;
     private final CustomSemaphore producingSemaphore;
 
@@ -14,6 +15,7 @@ class BoundedQueue<T>{
         this.maxSize = maxSize;
         producingSemaphore = new CustomSemaphore(maxSize);
         consumingSemaphore = new CustomSemaphore(0);
+        indices = new int[maxSize];
     }
 
     //Global variables --------------------------------------------------------------
@@ -30,11 +32,14 @@ class BoundedQueue<T>{
     4. Unlock the buffer
     5. Release a permit to the consumingSemaphore, which is used to control the number of items removed from the buffer
     */
-    public void add(T item) {
+    public int add(T item) {
+        int index = -1;
         try {
             producingSemaphore.acquire(); // Wait for space in the queue
             lock.lock(); // Lock the queue
             try {
+                index = findFirstEmptyIndex();
+                indices[index] = 1;
                 queue.offer(item); // Add the item to the queue
             } finally {
                 lock.unlock(); // Unlock the queue
@@ -43,6 +48,7 @@ class BoundedQueue<T>{
         } catch (Exception e) {
             System.err.println("Exception in add method: " + e.getMessage());
         }
+        return index;
     }
 
     //remove method --------------------------------------------------------------------
@@ -56,13 +62,14 @@ class BoundedQueue<T>{
     5. Release a permit to the producingSemaphore, which is used to control the number of items in the buffer
     6. Return the removed item
     */
-    public Object remove() {
+    public Object remove(int tableIndex) {
         Object item = null;
         try {
             consumingSemaphore.acquire(); // Wait for an item to be available
             lock.lock(); // Lock the queue
             try {
                 item = queue.poll(); // Remove and return the highest-priority item
+                indices[tableIndex] = 0;
             } finally {
                 lock.unlock(); // Unlock the queue
             }
@@ -73,6 +80,17 @@ class BoundedQueue<T>{
         return item;
     }
 
+    private int findFirstEmptyIndex() {
+        for (int i = 0; i < indices.length; i++) {
+            if (indices[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    
+
     //getters --------------------------------------------------------------------
     public boolean isFull() {
         return queue.size() == maxSize;
@@ -81,6 +99,11 @@ class BoundedQueue<T>{
     public boolean isEmpty() {
         return queue.isEmpty();
     }
+
+    public int size() {
+        return queue.size();
+    }
+    
 
     @Override
     public String toString() {
